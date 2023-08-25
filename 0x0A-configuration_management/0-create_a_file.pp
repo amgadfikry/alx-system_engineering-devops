42d717300d41428998dbd71debcacae25ba62874
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-25 21:07:19 +0300

    fix puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # puppet manifest that create file
 
-file { "school":
-	path => "/tmp/school",
-	mode => "0744",
-	owner => "www-data",
-	group => "www-data",
-	content => "I love Puppet"
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
