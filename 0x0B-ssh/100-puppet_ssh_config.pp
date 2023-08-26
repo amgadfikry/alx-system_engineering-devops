91e33f19eb271ee58246d019498c1bdd2cb88ed9
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-26 14:36:38 -0700

    use puupet to configure server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
 # change ssh config file
 file { 'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no'
+  path  => '/etc/ssh/ssh_config',
+  match => '	PasswordAuthentication yes',
+  line  => 'PasswordAuthentication no'
 }
 file { 'Declare identity file'}:
   path => '/etc/ssh/ssh_config',
