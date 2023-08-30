29d8fc237320db4eb66573c2baadd6faaa35531a
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-29 17:17:10 -0700

    puppet automation of install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,20 @@
 # puppet manifest for install package
 package { 'nginx':
-  ensure   => 'installed',
-  provider => 'apt_get'
+  ensure => installed,
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!'
-}
-
-file_line { 'config':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => '/server_name _;',
-  line   => 'rewrite ^/(.*)$ http://www.youtube.com permanent;'
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-  ensure => 'running',
+  ensure  => running,
+  require => Package['nginx'],
 }
