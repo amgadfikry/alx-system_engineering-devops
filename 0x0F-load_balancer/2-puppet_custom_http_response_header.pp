4a57b2108c698ab1e5ad3c7ea81268edbe5f49ab
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-04 17:44:25 -0700

    puppet manifest to ensure nginx response had header variable

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,7 +8,7 @@ file_line { 'redirect':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => 'rewrite ^/(.*)$ http://www.youtube.com permanent;\nadd_header X-Served-By $hostname;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;\nadd_header X-Served-By $hostname;',
 }
 
 file { '/var/www/html/index.html':
