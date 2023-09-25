3d4f48cd22f3181b4abcacee829e67374ce62708
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-25 23:30:07 +0300

    create python script get data from api and save as csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" module fot gather data from api """
+from requests import get
+from sys import argv
+
+
+def main():
+    """ main function to run as script not module """
+    url_tasks = "https://jsonplaceholder.typicode.com/todos"
+    tasks = get(url_tasks).json()
+    url_users = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    user = get(url_users).json().get("name")
+    with open("{}.csv".format(argv[1]), "a") as f:
+        for t in tasks:
+            if t.get("userId") == int(argv[1]):
+                line = '"{}","{}","{}","{}"\n'.format(
+                    argv[1], user, t.get("completed"), t.get("title"))
+                f.write(line)
+
+
+if __name__ == "__main__":
+    main()
