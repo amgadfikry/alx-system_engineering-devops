f02825bd3751133a55f66cbbe5f358333a934a76
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-10-10 21:29:05 +0300

    fix funtion

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,16 +7,14 @@ def top_ten(subreddit):
     """ get first ten hot posts on provided subreddit
         Params:
             subreddit: subreddit provided to search in it
-        Return:
-            print ten hot posts if valid subreddit
-            or print none if not vaild subreddit
     """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=9'
     header = {'User-Agent': 'Amgad_fikry_alx'}
     req = get(url, allow_redirects=False, headers=header)
     if req.status_code == 200:
-        data = req.json().get('data').get('children')
-        for i in range(10):
-            print(data[i].get('data').get('title'))
+        data = req.json()
+        posts = data.get('data').get('children')
+        for post in posts:
+            print(post.get('data').get('title'))
     else:
         print('None')
