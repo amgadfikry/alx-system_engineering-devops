e7bc78d498c34b48c0c714c3223fd9d96220f034
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-07-07 21:27:18 +0300

    add pupet file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,36 @@
-class custom_http_header {
-  package { 'nginx':
-    ensure => installed,
-  }
+# This Puppet manifest installs Nginx and adds a custom HTTP header "X-Served-By" with the server's hostname
 
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    subscribe  => File['/etc/nginx/sites-available/default'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('custom_http/default.erb'),
-    notify  => Service['nginx'],
-  }
+package { 'nginx':
+  ensure => installed,
 }
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => inline_template(@("EOF")),
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        add_header X-Served-By "<%= @hostname %>";
+        try_files \$uri \$uri/ =404;
+    }
+}
+  | EOF
+  ,
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
+
+# Get the hostname fact to use in the template
+$hostname = $facts['networking']['hostname']
